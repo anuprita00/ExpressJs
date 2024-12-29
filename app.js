823ffa05dc3bcc23645fd0e19d71cc7fa962f14f
Anuprita <anupande84@gmail.com>
2024-12-29 09:35:50 +0530

    Parsing incoming request body

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use((req, res, next) =>{  //use allows us to add middleware functions
-//     console.log("In the middleware!");
-//     next(); //Allows the request to continue to the next middleware in line
-// });
+//for parsing incoming request body. add it before route handling middleware
+//do npm i --save body-parser
+//parses URL-encoded form data into a JavaScript object and disables parsing of extended data structures.
+app.use(bodyParser.urlencoded({extended:false}));
 
 //Handling routes
 app.use('/add-product',(req, res, next) => {
-    console.log("In another middleware!");
-    res.send("<h1>The 'Add Product' page</h1>"); //send a response
+    res.send(`<html>
+        <form action="/product" method="POST">
+        <input type ="text" name ="title">
+        <button type="submit">Add Product</button> 
+        </form></html>`);  
+});
+
+app.use('/product',(req, res) =>{
+    console.log(req.body); //logs form data 
+    res.redirect('/');// for redirecting to the routes
 });
 
 app.use('/',(req, res, next) => {
-    console.log("In another middleware!");
     res.send("<h1>Hello from Express JS!</h1>"); //send a response
 });
 
