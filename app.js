d94994a0489174891a56323b61cc079e8d6a5718
Anuprita <anupande84@gmail.com>
2025-01-03 12:56:03 +0530

    Adding main-layout

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(shopRoutes);
 //gives 404 page for wrong /
 app.use((req, res, next) => {
    // res.status(404).sendFile(path.join( __dirname, 'views', '404.html'));
-   res.status(404).render('404');
+   res.status(404).render('404',{pageTitle: 'Page Not Found!'});
 });
 
 app.listen(3000);
